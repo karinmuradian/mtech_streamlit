d61a69ec35f3258599895b2443d64e3cffd18f49
Karina <134699541+karinmuradian@users.noreply.github.com>
2023-12-13 19:38:37 +0300

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -38,6 +38,13 @@ csv_data = load_data()
 #Просмотр откорректированного файла
 
 show_data = st.button('Показать данные')
+
+if "show_data_state" not in st.session_state:
+    st.session_state.show_data_state = False
+
+if show_data or st.session_state.show_data_state:
+    st.session_state.show_data_state = True
+    
 if show_data == True:
     st.subheader('Загруженные данные')
     st.dataframe(csv_data)
